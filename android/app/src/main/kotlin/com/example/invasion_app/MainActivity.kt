936609b52f67501bb9c57885300392d8b430cfe3
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2022-09-29 23:18:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/invasion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.invasion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/character.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'character.freezed.dart';
+part 'character.g.dart';
+
+@freezed
+class Character with _$Character {
+  factory Character({
+    required String name,
+    required String gender,
+  }) = _Character;
+
+  factory Character.fromJson(Map<String, dynamic> json) =>
+      _$CharacterFromJson(json);
+}
